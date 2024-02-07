f279a16657ee250937f0941b3ccc4f614a3857d6
CryptoBro <123989939+CryptoBro44@users.noreply.github.com>
2024-02-07 19:44:57 +0100

    Add files via upload

---
+++ portScan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function ctrl_c(){
+        echo -e "\n\n Saliendo... \n"
+        exit 1
+}
+
+#Ctrl_C
+trap ctrl_c INT
+
+for port in $(seq 1 65535); do
+        (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "[+] $port - OPEN"
+
+done
+
+echo "Terminado"
